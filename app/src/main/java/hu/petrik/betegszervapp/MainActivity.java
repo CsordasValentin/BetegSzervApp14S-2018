ab16be3395751001a4c312f64279058cb5c51809
Hali <hgabor@level14.hu>
2018-01-31 17:02:01 +0100

    Listas megjelenes, 2 tesztadat

--- app/src/main/java/hu/petrik/betegszervapp/MainActivity.java
+++ app/src/main/java/hu/petrik/betegszervapp/MainActivity.java
@@ -1,11 +1,16 @@
 package hu.petrik.betegszervapp;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             db.execSQL("CREATE TABLE beteg (\n" +
                     "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
                     "    nev TEXT NOT NULL,\n" +
+                    "    taj TEXT NOT NULL,\n" +
                     "    szerv TEXT NOT NULL,\n" +
                     "    tipus TEXT NOT NULL,\n" +
                     "    szerv_id INTEGER,\n" +
@@ -47,11 +53,48 @@ public class MainActivity extends AppCompatActivity {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                DbHelper helper = new DbHelper(getApplicationContext());
+                DbHelper helper = new DbHelper(MainActivity.this);
                 db = helper.getWritableDatabase();
-                
-                // Feltöltjük a listát adatokkal
+
+                ListView betegLista = (ListView)findViewById(R.id.beteg_lista);
+
+                ArrayList<String> lista = new ArrayList<>();
+                Cursor cursor = db.query(
+                        "beteg",
+                        new String[] { "nev", "taj" },
+                        null,
+                        null,
+                        null,
+                        null,
+                        "nev",
+                        null);
+
+                while (cursor.moveToNext()) {
+                    String nev = cursor.getString(0);
+                    String taj = cursor.getString(1);
+                    String sor = nev + " (" + taj + ")";
+                    lista.add(sor);
+                }
+                cursor.close();
+
+                lista.add("Kovacs Bela (123-456-789)");
+                lista.add("Aasdf Jkl; (111-222-333)");
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        lista
+                );
+
+                betegLista.setAdapter(adapter);
             }
         });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
     }
 }
